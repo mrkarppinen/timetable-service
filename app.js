2ae8dc10398635abdd9a634baa485e47f42b3d80
Jussi Karppinen <jkarppin@Jussi-MacBook-Pro.local>
2018-10-05 15:42:34 +0300

    enabled cors

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 const lineApi = require('./api/line');
 const port = process.env.PORT || 3001;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/api', lineApi);
 
